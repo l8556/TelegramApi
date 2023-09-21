8a289cddd3d08ba4182a0c9e41740f2632d8270f
l02 <l8556@outlook.com>
2023-09-21 18:32:23 +0300

    minor refactoring

---
+++ telegram/Auth.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from os import environ
+from os.path import isfile, join, expanduser
+
+from host_control import File
+
+
+class Auth:
+
+    def __init__(self, token: str = None, chat_id: str = None):
+        self._telegram_dir = join(expanduser('~'), '.telegram')
+        self.token = token if token else self._get_token()
+        self.chat_id = chat_id if chat_id else self._get_chat_id()
+
+    def _get_token(self) -> "str | None":
+        token_file = join(self._telegram_dir, 'token')
+
+        if token_file and isfile(token_file):
+            return File.read(token_file).strip()
+        elif environ.get('TELEGRAM_TOKEN', False):
+            return environ.get('TELEGRAM_TOKEN')
+
+        print(f"[cyan]|INFO| Telegram token not exists.")
+
+    def _get_chat_id(self) -> "str | None":
+        chat_id_file = join(self._telegram_dir, 'chat')
+
+        if isfile(chat_id_file):
+            return File.read(chat_id_file).strip()
+        elif environ.get('CHANNEL_ID', False):
+            return environ.get('CHANNEL_ID')
+
+        print(f"[cyan]|INFO| Telegram chat id not exists.")
