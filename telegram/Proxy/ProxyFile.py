05c8b791b1a0dadac643ae04e599faa269c410c4
l02 <l8556@outlook.com>
2024-02-02 13:40:33 +0300

    code optimisation

--- telegram/Proxy/ProxyFile.py
+++ telegram/Proxy/ProxyFile.py
@@ -8,6 +8,7 @@ from .Proxy import Proxy
 
 class ProxyFile:
     _checked_files = {}
+    _checked_paths = {}
     _default_proxy_file = join(expanduser('~'), '.telegram', 'proxy.json')
 
     def __init__(self, path: str = None):
@@ -15,20 +16,31 @@ class ProxyFile:
 
 
     def _get_proxy_file(self, path: str = None) -> str:
+        file_key = path if path else 'None'
+
+        if file_key in ProxyFile._checked_paths:
+            return ProxyFile._checked_paths[file_key]
+
         if path and isinstance(path, str) and isfile(path):
-            return path
+            result = path
         elif isfile(self._default_proxy_file):
-            return self._default_proxy_file
-        print(f"[red]Proxy configuration file not found")
+            result = self._default_proxy_file
+        else:
+            print(f"[red]Proxy configuration file not found")
+            result = None
+
+        ProxyFile._checked_paths[file_key] = result
+
+        return result
 
     def get_configs(self) -> dict:
         result = {}
         file_key = self._proxy_file if self._proxy_file else 'None'
 
-        if self._proxy_file:
-            if file_key in ProxyFile._checked_files:
-                return ProxyFile._checked_files[file_key]
+        if file_key in ProxyFile._checked_files:
+            return ProxyFile._checked_files[file_key]
 
+        if self._proxy_file:
             config = File.read_json(self._proxy_file)
             if self._check_config(config):
                 proxy = Proxy(login=config['login'], password=config['password'], ip=config['ip'], port=config['port'])
@@ -45,3 +57,8 @@ class ProxyFile:
                 print(f"[red]|ERROR| Empty parameter {key} in proxy.json file")
                 return False
         return True
+
+if __name__ == '__main__':
+    ProxyFile().get_configs()
+    ProxyFile().get_configs()
+    ProxyFile().get_configs()
