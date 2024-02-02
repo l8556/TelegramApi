5d7c1cbd936f85990cd0d71a7cb26d7bb502bf7c
l02 <l8556@outlook.com>
2024-02-02 14:08:48 +0300

    minor refactoring

--- telegram/Proxy/ProxyFile.py
+++ telegram/Proxy/ProxyFile.py
@@ -2,6 +2,7 @@
 from rich import print
 from os.path import join, expanduser, isfile
 from host_tools import File
+from typing import Optional
 
 from .Proxy import Proxy
 
@@ -11,15 +12,15 @@ class ProxyFile:
     _checked_paths = {}
     _default_proxy_file = join(expanduser('~'), '.telegram', 'proxy.json')
 
-    def __init__(self, path: str = None):
+    def __init__(self, path: Optional[str] = None):
         self._proxy_file = self._get_proxy_file(path)
 
 
-    def _get_proxy_file(self, path: str = None) -> str:
+    def _get_proxy_file(self, path: Optional[str] = None) -> Optional[str]:
         file_key = path if path else 'None'
 
-        if file_key in ProxyFile._checked_paths:
-            return ProxyFile._checked_paths[file_key]
+        if file_key in self._checked_paths:
+            return self._checked_paths[file_key]
 
         if path and isinstance(path, str) and isfile(path):
             result = path
@@ -29,7 +30,7 @@ class ProxyFile:
             print(f"[red]Proxy configuration file not found")
             result = None
 
-        ProxyFile._checked_paths[file_key] = result
+        self._checked_paths[file_key] = result
 
         return result
 
@@ -37,8 +38,8 @@ class ProxyFile:
         result = {}
         file_key = self._proxy_file if self._proxy_file else 'None'
 
-        if file_key in ProxyFile._checked_files:
-            return ProxyFile._checked_files[file_key]
+        if file_key in self._checked_files:
+            return self._checked_files[file_key]
 
         if self._proxy_file:
             config = File.read_json(self._proxy_file)
@@ -46,7 +47,7 @@ class ProxyFile:
                 proxy = Proxy(login=config['login'], password=config['password'], ip=config['ip'], port=config['port'])
                 result = proxy.configs
 
-        ProxyFile._checked_files[file_key] = result
+        self._checked_files[file_key] = result
 
         return result
 
@@ -57,8 +58,3 @@ class ProxyFile:
                 print(f"[red]|ERROR| Empty parameter {key} in proxy.json file")
                 return False
         return True
-
-if __name__ == '__main__':
-    ProxyFile().get_configs()
-    ProxyFile().get_configs()
-    ProxyFile().get_configs()
