c75ff50e32aef6fb36054eaaff0b84cbfaf19d8b
l02 <l8556@outlook.com>
2024-03-25 14:11:49 +0300

    minor refactoring

--- telegram/Proxy/ProxyChecker.py
+++ telegram/Proxy/ProxyChecker.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from host_tools.utils import Str
 from requests import post
 from requests.exceptions import RequestException
 
@@ -16,7 +17,7 @@ class ProxyChecker:
             return ProxyChecker._checked_proxies[proxy_key]
 
         try:
-            post(Config.API_HOST, proxies=proxies)
+            post(f"{Str.delete_last_slash(Config.API_HOST)}/", proxies=proxies)
             result = True
         except RequestException:
             result = False
--- telegram/config.py
+++ telegram/config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Config:
-    API_HOST: str = 'https://api.telegram.org'
+    API_HOST: str = 'https://api.telegram.org' # Without last slash
     MAX_DOCUMENT_SIZE: int = 50_000_000
     MAX_CAPTCHA_LENGTH: int = 1000
     MAX_MESSAGE_LENGTH: int = 4096
