f3083aebe1bde87800e959327424accc11a77138
l02 <l8556@outlook.com>
2024-05-09 18:58:36 +0300

    .

--- telegram/Send/Caption.py
+++ telegram/Send/Caption.py
@@ -3,9 +3,7 @@ from telegram.config import Config
 
 
 class Caption:
-
-    def __init__(self):
-        self.captcha_length = Config.MAX_CAPTCHA_LENGTH
+    _captcha_length: int = Config.MAX_CAPTCHA_LENGTH
 
     def prepare(self, caption: str) -> str:
-        return caption[:self.captcha_length]
+        return caption[:self._captcha_length]
