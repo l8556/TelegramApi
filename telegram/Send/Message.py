cd032cac807cf7cdb8ba21b390634da885a97218
l02 <l8556@outlook.com>
2024-05-09 17:03:33 +0300

    .

--- telegram/Send/Message.py
+++ telegram/Send/Message.py
@@ -6,18 +6,23 @@ from ..utils import File
 
 from .Document import Document
 from .Send import Send
+from .telegram_request import TelegramRequests
 
 
 class Message(Send):
+
+    def __init__(self, requests: TelegramRequests):
+        super().__init__(requests=requests)
+        self.document = Document(self.requests)
 
     def send(self, message: str, out_msg: bool = False, parse_mode: str = None):
         _parse_mod = parse_mode if parse_mode else self.__DEFAULT_PARSE_MOD
         print(message) if out_msg else ...
 
         if len(message) > self.__MAX_MESSAGE_LENGTH:
-            document = self.make_doc(message=message)
-            Document(self.requests).send(document, caption=message)
-            return File.delete(document)
+            document_path = self.make_doc(message=message)
+            self.document.send(document_path, caption=message)
+            return File.delete(document_path)
 
         message_data = {"chat_id": self.requests.auth.chat_id, "text": message, "parse_mode": _parse_mod}
         self.requests.post('sendMessage', data=message_data, tg_log=False)
